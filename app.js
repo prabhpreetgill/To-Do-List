9c14becc7d124917106d719aab479d49342ee6b6
prabhpreetgill <116679689+prabhpreetgill@users.noreply.github.com>
2023-05-10 19:42:07 -0400

    fixed db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+mongoose.connect('mongodb+srv://admin-prabh:Devils30.@cluster0.nfztjmo.mongodb.net/todolistDB?retryWrites=true&w=majority');
 
 const itemsSchema = {
   name: String
