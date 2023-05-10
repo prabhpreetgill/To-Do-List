47e54e6cf0afd7021b4f702dc984127cfb1f96a1
Prabh Gill <116679689+prabhpreetgill@users.noreply.github.com>
2023-05-09 22:15:34 -0400

    changes to port

--- app.js
+++ app.js
@@ -80,12 +80,6 @@ app.post("/delete", (req, res) => {
   });
 })
 
-let port = process.env.PORT;
-if(port == null || port ==""){
-  port = 3000;
-}
-
-app.listen(port, function () {
-  console.log("Server Working");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
